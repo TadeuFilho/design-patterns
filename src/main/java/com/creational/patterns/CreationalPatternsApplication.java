93dfe572438468401c8e781f51a87b187ff891b1
Tadeu Filho <jose.vanderlei@fcamara.com.br>
2022-11-12 10:42:44 -0300

    Padrão Builder

---
+++ src/main/java/com/creational/patterns/CreationalPatternsApplication.java
@@ -0,0 +1,18 @@
+package com.creational.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalPatternsApplication {
+	/*
+	*
+	* project created to exemplify creational patterns
+	*
+	* */
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationalPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creational/patterns/CreationalPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creational.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationalPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
